e3a6d7435d3791f3f9e0bc19ca09b9146164fdd3
ShodaMashin <37033266+ShodaMashin@users.noreply.github.com>
2019-06-06 13:21:24 +1000

    landing page done

--- web/__init__.py
+++ web/__init__.py
@@ -4,64 +4,36 @@ from datetime import datetime
 app = Flask(__name__)
 app.secret_key = b'iunew842o3vu8n'
 
-
-@app.route("/logout", methods=['GET', 'POST'])
-def logout():
-    if 'username' in session:
-        session.pop('username', None)
-    return redirect(url_for("login"))
-
-
-@app.route("/login", methods=['GET', 'POST'])
-def login(invalid=False):
-    if request.method == 'POST':
-        if request.form['username'] == 'admin' and \
-                request.form['password'] == 'bongo':
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
-        else:
-            return render_template("login.html", invalid=True)
-    return render_template("login.html")
-
-
 @app.route("/")
 def index():
-    if 'username' in session:
-        return render_template("index.html")
-    return redirect(url_for('login'))
+    return render_template("index.html")
 
 
 @app.route("/analytics")
 def analytics():
-    if 'username' in session:
-        return render_template("analytics.html")
-    return redirect(url_for('login'))
+    return render_template("analytics.html")
 
 
 @app.route("/books")
 def books(error=None):
-    if 'username' in session:
-        return render_template("books.html")
-    return redirect(url_for('login'))
+    return render_template("books.html")
 
 
 @app.route("/book", methods=['GET', 'POST'])
 def book():
-    if 'username' in session:
-        error = None
-        if request.method == 'POST':
-            error = False
-            title = request.form['title']
-            author = request.form['author']
-            isbn = request.form['isbn']
-            try:
-                published = datetime.strptime(request.form["published"], '%Y-%m-%d')
-            except ValueError:
-                error = True
-            if error == False:
-                return render_template("books.html", error=error)
-        return render_template("addBook.html", error=error)
-    return redirect(url_for('login'))
+    error = None
+    if request.method == 'POST':
+        error = False
+        title = request.form['title']
+        author = request.form['author']
+        isbn = request.form['isbn']
+        try:
+            published = datetime.strptime(request.form["published"], '%Y-%m-%d')
+        except ValueError:
+            error = True
+        if error == False:
+            return render_template("books.html", error=error)
+    return render_template("addBook.html", error=error)
 
 
 def start():
